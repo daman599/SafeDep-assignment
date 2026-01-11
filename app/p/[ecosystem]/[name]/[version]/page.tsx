479108d2a1eaf5886a016ba065ecc5867cf5a0ea
daman599 <damanpreetk117@gmail.com>
2026-01-11 23:45:57 +0530

    few fixes

--- app/p/[ecosystem]/[name]/[version]/page.tsx
+++ app/p/[ecosystem]/[name]/[version]/page.tsx
@@ -40,7 +40,7 @@ type packageDataType = {
     }
 }
 
-const PackageDetails = ({ title, children }: { title: string, children: React.ReactNode }) => {
+const PackageDetails = ({ title, children = "NA" }: { title: string, children: React.ReactNode }) => {
     return (
         <div className="flex items-center justify-center gap-1">
             <div className="size-2 bg-[#069c85] rounded-full animate-pulse"></div>
@@ -48,6 +48,14 @@ const PackageDetails = ({ title, children }: { title: string, children: React.Re
             {children}
         </div >
     );
+}
+
+const formatDate = (date: string) => {
+    return new Date(date).toLocaleString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    })
 }
 
 export default function PackageData() {
@@ -94,29 +102,43 @@ export default function PackageData() {
                     </PackageDetails>
 
                     <PackageDetails title={"Published Date"}>
-                        <span className="text-base font-medium text-gray-200/50">{packageData?.insight.packagePublishedAt}</span>
+                        <span className="text-base font-medium text-gray-200/50">
+                            {packageData?.insight?.packagePublishedAt ?
+                                formatDate(packageData?.insight.packagePublishedAt)
+                                : "Unknown"}
+                        </span>
                     </PackageDetails>
 
                     <PackageDetails title={"Registries"}>
-                        <a href={packageData?.insight.registries[0]} className="text-base font-medium text-gray-200/50" >{packageData?.insight.registries[0]} </a>
+                        {packageData?.insight?.registries?.length > 0 ? (
+                            <a href={packageData?.insight.registries[0]} className="text-base font-medium text-gray-200/50" >
+                                {packageData?.insight.registries[0]}
+                            </a>
+                        ) : (
+                            <span className="text-base font-medium text-gray-200/50">Not available</span>
+                        )}
                     </PackageDetails>
 
                     <PackageDetails title={"Dependencies"}>
-                        <span className="text-base font-medium text-gray-200/50">Total {packageData?.insight.dependencies.length} dependencies</span>
+                        <span className="text-base font-medium text-gray-200/50">
+                            Total {packageData?.insight?.dependencies?.length ?? 0} dependencies
+                        </span>
                     </PackageDetails>
 
-                    <div className="flex flex-col items-start justify-center gap-1">
+                    < div className="flex flex-col items-start justify-center gap-1">
                         <div className="flex items-center justify-center gap-1">
                             <div className="size-2 bg-[#069c85] rounded-full animate-pulse"></div>
                             <span className="text-gray-300 text-lg font-bold">Vulnerabilities: </span>
                         </div>
-
-                        {packageData?.insight.vulnerabilities.map((vul, i) => (
-                            <div key={i} className="flex items-center justify-center gap-1 ml-7">
-                                <div className="size-2 bg-orange-600/50 rounded-full "></div>
-                                <span className="text-base font-medium text-gray-200/50">{vul.summary}</span>
-                            </div>
-                        ))}
+                        {packageData?.insight?.vulnerabilities?.length > 0 ?
+                            packageData?.insight.vulnerabilities.map((vul, i) => (
+                                <div key={i} className="flex items-center justify-center gap-1 ml-7">
+                                    <div className="size-2 bg-orange-600/50 rounded-full "></div>
+                                    <span className="text-base font-medium text-gray-200/50">{vul.summary}</span>
+                                </div>
+                            ))
+                            : <span className="text-base font-medium text-gray-200/50">There are no vulnerabilities in this package.</span>
+                        }
                     </div>
 
                     <div className="flex flex-col items-start justify-center gap-1">
@@ -125,15 +147,16 @@ export default function PackageData() {
                             <span className="text-gray-300 text-lg font-bold">Project Insights:</span>
                         </div>
 
-                        {packageData?.insight.projectInsights.map((insight, i) =>
-                            <div key={i} className="flex flex-col items-start justify-center gap-1 ml-7">
-                                <span className="text-base font-medium text-gray-200/50"> Forks: {insight.forks}</span>
-                                <span className="text-base font-medium text-gray-200/50"> Stars : {insight.stars}</span>
-                                <span className="text-base font-medium text-gray-200/50"> Open Issues: {insight.issues.open}</span>
-                                <span className="text-base font-medium text-gray-200/50"> Url : <a href={insight.project.url}>
-                                    {packageData?.insight.projectInsights[0].project.url}</a></span>
-                            </div>
-                        )}
+                        {packageData?.insight?.projectInsights?.length > 0 ?
+                            packageData?.insight.projectInsights.map((insight, i) =>
+                                <div key={i} className="flex flex-col items-start justify-center gap-1 ml-7">
+                                    <span className="text-base font-medium text-gray-200/50"> Forks: {insight?.forks ?? "Not mentioned"}</span>
+                                    <span className="text-base font-medium text-gray-200/50"> Stars : {insight?.stars ?? "Not mentioned"}</span>
+                                    <span className="text-base font-medium text-gray-200/50"> Open Issues: {insight?.issues?.open ?? "Not mentioned"}</span>
+                                    <span className="text-base font-medium text-gray-200/50"> Url : <a href={insight?.project?.url ?? "Not given"}>
+                                        {packageData?.insight.projectInsights[0].project.url}</a></span>
+                                </div>
+                            ) : <span className="text-base font-medium text-gray-200/50">There are no insights about project.</span>}
                     </div>
                 </div>
             </div>
